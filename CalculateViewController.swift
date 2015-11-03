ff51823baa077b08b9e18d72fa62039697757f9b
eliasbloemendaal <eliasbloemendaal@gmail.com>
2015-11-03 11:22:57 +0100

    fill everything in

--- CalculateViewController.swift
+++ CalculateViewController.swift
@@ -10,11 +10,58 @@ import UIKit
 
 class CalculateViewController: UIViewController {
 
+    var element : Double = 1.0
+    var valuta : Double = 1.0
     
+    @IBOutlet weak var DifferentValuta: UISegmentedControl!
+    @IBOutlet weak var DifferenElements: UISegmentedControl!
+    @IBOutlet weak var userinputKilogram: UITextField!
+    @IBOutlet weak var userinputSeamiles: UITextField!
+    @IBOutlet weak var TotalPriceScreen: UILabel!
+    @IBOutlet weak var PriceButton: UIButton!
+    
+    @IBAction func elements(sender: AnyObject) {
+        let index : Int = DifferenElements.selectedSegmentIndex
+        
+        if index == 0 {
+            element = 0.3
+        }
+        else if index == 1 {
+            element = 0.4
+        }
+        else {
+            element = 0.5
+        }
+    }
+    
+    @IBAction func Valutas(sender: AnyObject) {
+        let index : Int = DifferentValuta.selectedSegmentIndex
+        
+        if index == 0 {
+            valuta = 1
+        }
+        else{
+            valuta = 1.1033 
+        }
+    }
+    
+    @IBAction func ScreenPriceButton(sender: UIButton) {
+        if userinputKilogram.text == ""  {
+            TotalPriceScreen.text = String("Fill everything in!")
+        }
+        else if userinputSeamiles.text == "" {
+            TotalPriceScreen.text = String("Fill everything in!")
+        }
+        else{
+        let totalprice : Double =  element * valuta * Double(userinputKilogram.text!)! * Double(userinputSeamiles.text!)!
+        TotalPriceScreen.text = String(totalprice)
+        }
+    }
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.PriceButton.setTitle("PRICE", forState: UIControlState.Normal)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,7 +69,6 @@ class CalculateViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
     /*
     // MARK: - Navigation
 
